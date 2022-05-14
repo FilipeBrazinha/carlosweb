d7baa88d56ec862714b5d8ffac77a7cc79b43919
FilipeRamos <filipebrazinha@gmail.com>
2022-05-14 22:02:57 +0100

    form

--- contactform.php
+++ contactform.php
@@ -4,15 +4,16 @@ if(isset($_POST["submit"])){
   $name= $_POST["name"];
   $email= $_POST["email"];
   $subject= $_POST["subject"];
+  $phone= $_POST["phone"];
   $message= $_POST["message"];
 
 $mailheader = "From:".$name. "<".$email.">\r\n";
 
 $recipient = "filipebrazinha@gmail.com";
 
-mail($recipient, $subject, $message, $mailheader)
+mail($recipient,$subject,$phone,$message,$mailheader)
 or die("error");
 
 echo"message send";
-
+}
 ?>
